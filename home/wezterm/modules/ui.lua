5628fbd87e6ac8d1acb70ab3effb002469b3a67e
DotRedstone <2724236945@qq.com>
2026-06-18 16:04:28 +0800

    fix(wezterm): 收紧触屏终端界面

--- home/wezterm/modules/ui.lua
+++ home/wezterm/modules/ui.lua
@@ -9,8 +9,8 @@ local M = {}
 function M.apply(config, _)
   -- [GNOME tablet UI]
   config.enable_tab_bar = true
-  config.hide_tab_bar_if_only_one_tab = false
-  config.use_fancy_tab_bar = true
+  config.hide_tab_bar_if_only_one_tab = true
+  config.use_fancy_tab_bar = false
   config.tab_bar_at_bottom = false
   config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
   config.integrated_title_button_style = "Gnome"
@@ -18,17 +18,18 @@ function M.apply(config, _)
   config.integrated_title_buttons = { "Hide", "Maximize", "Close" }
   config.integrated_title_button_color = "Auto"
   config.show_tab_index_in_tab_bar = false
+  config.tab_max_width = 18
   config.window_padding = {
-    left = "1.75cell",
-    right = "1.75cell",
-    top = "0.65cell",
-    bottom = "0.9cell",
+    left = "1.0cell",
+    right = "1.0cell",
+    top = "0.35cell",
+    bottom = "0.45cell",
   }
   config.window_close_confirmation = "AlwaysPrompt"
   config.adjust_window_size_when_changing_font_size = false
   config.initial_cols = 96
   config.initial_rows = 28
-  config.enable_scroll_bar = true
+  config.enable_scroll_bar = false
 end
 
 return M
