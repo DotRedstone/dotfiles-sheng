3b7c7d9acf64fcda36f1db674b10f8305a044b81
DotRedstone <2724236945@qq.com>
2026-06-18 11:56:24 +0800

    fix(rime): 链接 Rime Ice 输入方案数据

--- home/fcitx5/rime/data.nix
+++ home/fcitx5/rime/data.nix
@@ -1,13 +1,54 @@
 # ---
 # Module: Rime Data
-# Description: Symlinking external Rime data (like rime-ice lua scripts)
+# Description: Symlinking external Rime Ice data files
 # Scope: Home Manager
 # ---
 
-{ pkgs, ... }: {
-  home.file.".local/share/fcitx5/rime/lua" = {
-    source = "${pkgs.rime-ice}/share/rime-data/lua";
+{ lib, pkgs, ... }:
+let
+  rimeIce = "${pkgs.rime-ice}/share/rime-data";
+
+  linkedDirectories = [
+    "cn_dicts"
+    "en_dicts"
+    "lua"
+    "opencc"
+  ];
+
+  linkedFiles = [
+    "custom_phrase.txt"
+    "double_pinyin.schema.yaml"
+    "double_pinyin_abc.schema.yaml"
+    "double_pinyin_flypy.schema.yaml"
+    "double_pinyin_jiajia.schema.yaml"
+    "double_pinyin_mspy.schema.yaml"
+    "double_pinyin_sogou.schema.yaml"
+    "double_pinyin_ziguang.schema.yaml"
+    "melt_eng.dict.yaml"
+    "melt_eng.schema.yaml"
+    "radical_pinyin.dict.yaml"
+    "radical_pinyin.schema.yaml"
+    "rime_ice.dict.yaml"
+    "rime_ice.schema.yaml"
+    "rime_ice_suggestion.yaml"
+    "symbols_caps_v.yaml"
+    "symbols_v.yaml"
+    "t9.schema.yaml"
+  ];
+
+  directoryLinks = lib.genAttrs linkedDirectories (name: {
+    source = "${rimeIce}/${name}";
     recursive = true;
     force = true;
-  };
+  });
+
+  fileLinks = lib.genAttrs linkedFiles (name: {
+    source = "${rimeIce}/${name}";
+    force = true;
+  });
+in
+{
+  home.file =
+    lib.mapAttrs' (name: value: lib.nameValuePair ".local/share/fcitx5/rime/${name}" value)
+      (directoryLinks // fileLinks);
 }
