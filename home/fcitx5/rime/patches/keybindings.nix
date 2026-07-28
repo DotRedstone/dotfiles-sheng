c1cd7911ae067f38260aeaf0103c17606857413c
dotredstone <2724236945@qq.com>
2026-07-28 19:54:41 +0800

    fix(fcitx5): remove invalid Rime noop bindings

--- home/fcitx5/rime/patches/keybindings.nix
+++ home/fcitx5/rime/patches/keybindings.nix
@@ -8,7 +8,5 @@
   "key_binder/bindings/+" = [
     { when = "has_menu"; accept = "semicolon"; send = "2"; }
     { when = "has_menu"; accept = "apostrophe"; send = "3"; }
-    { when = "has_menu"; accept = "Control_L"; send = "noop"; }
-    { when = "has_menu"; accept = "Control_R"; send = "noop"; }
   ];
 }
