3d1e2f599e868daa377df5b30970c2838fe09528
dotredstone <2724236945@qq.com>
2026-07-28 19:57:13 +0800

    fix(fcitx5): keep Rime sync backups writable

--- home/fcitx5/rime/default.nix
+++ home/fcitx5/rime/default.nix
@@ -15,4 +15,12 @@
     (pkgs.formats.yaml { }).generate "rime_ice.custom.yaml" {
       patch = import ./patches { inherit lib; };
     };
+
+  home.activation.rimeSyncPermissions = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    sync_root="$HOME/.local/share/fcitx5/rime/sync"
+    if [ -d "$sync_root" ]; then
+      ${pkgs.findutils}/bin/find "$sync_root" -type f \
+        -exec ${pkgs.coreutils}/bin/chmod u+w {} +
+    fi
+  '';
 }
