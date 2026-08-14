5711834bfcc5a5c922a31fd792e2171f3edc9761
dotredstone <2724236945@qq.com>
2026-08-14 09:01:46 +0800

    chore(sheng): 锁定 v0.2.1 硬件平台

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     # 引用 sheng 硬件仓库。普通用户只需要 clone 本仓库，
     # 不需要在本地额外 clone nixos-sheng。
     nixos-sheng = {
-      url = "github:DotRedstone/nixos-sheng/v0.1.4?dir=nixos";
+      url = "github:DotRedstone/nixos-sheng/v0.2.1?dir=nixos";
       inputs.shengFirmware.follows = "shengFirmware";
     };
 
