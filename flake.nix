660b7522ed461ca81f4d92a4a62f7244f75fd387
dotredstone <2724236945@qq.com>
2026-08-25 20:18:48 +0800

    feat(sheng): enable fingerprint wake unlock

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     # 引用 sheng 硬件仓库。普通用户只需要 clone 本仓库，
     # 不需要在本地额外 clone nixos-sheng。
     nixos-sheng = {
-      url = "github:DotRedstone/nixos-sheng/0c91ac008aaa80944debfcc0bc83dd36c38eee00?dir=nixos";
+      url = "github:DotRedstone/nixos-sheng/4e0bba54b36d7e513e6ac5869bc08e166c4bfdee?dir=nixos";
       inputs.shengFirmware.follows = "shengFirmware";
     };
 
