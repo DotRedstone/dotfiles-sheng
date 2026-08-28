5df9e4ad4a218730daa601d43569cdc2f4ebb33b
dotredstone <2724236945@qq.com>
2026-08-29 07:14:46 +0800

    fix(input): deploy corrected Focus Pen pointer diagnostics

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     # 引用 sheng 硬件仓库。普通用户只需要 clone 本仓库，
     # 不需要在本地额外 clone nixos-sheng。
     nixos-sheng = {
-      url = "github:DotRedstone/nixos-sheng/b6e3168e2121d36d12fc3e951dfd35303f1fa6f4?dir=nixos";
+      url = "github:DotRedstone/nixos-sheng/757a72f52a6796f46dced8e9a54ff37c5207874c?dir=nixos";
       inputs.shengFirmware.follows = "shengFirmware";
     };
 
