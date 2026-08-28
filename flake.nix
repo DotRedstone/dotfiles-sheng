8a626b9e119fab05385324de83ce7e2aba9307a1
dotredstone <2724236945@qq.com>
2026-08-29 00:42:29 +0800

    fix(sheng): use quiet Focus Pen startup

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     # 引用 sheng 硬件仓库。普通用户只需要 clone 本仓库，
     # 不需要在本地额外 clone nixos-sheng。
     nixos-sheng = {
-      url = "github:DotRedstone/nixos-sheng/4ec12bdfe305617dbee8d24dd5cbf6a5103f48ad?dir=nixos";
+      url = "github:DotRedstone/nixos-sheng/2cba9b35d0e42da9e8e38c6331096add1ec8e16d?dir=nixos";
       inputs.shengFirmware.follows = "shengFirmware";
     };
 
