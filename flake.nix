6f22bc8a0ce15db129e25bf16c94f5783f550e5a
dotredstone <2724236945@qq.com>
2026-08-31 10:43:16 +0800

    perf: pin interactive build policy

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     # 引用 sheng 硬件仓库。普通用户只需要 clone 本仓库，
     # 不需要在本地额外 clone nixos-sheng。
     nixos-sheng = {
-      url = "github:DotRedstone/nixos-sheng/1d947ecab83a8722e52c8f23bb76f0e055243189?dir=nixos";
+      url = "github:DotRedstone/nixos-sheng/5622cd62ebb35f051fd6d56148e7e9b29cf7c817?dir=nixos";
       inputs.shengFirmware.follows = "shengFirmware";
     };
 
