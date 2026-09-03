acad4e57573a52ed0d891e4caff63db2c52a1ff0
dotredstone <2724236945@qq.com>
2026-09-03 11:46:02 +0800

    fix(sheng): select offline charging before desktop

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     # 引用 sheng 硬件仓库。普通用户只需要 clone 本仓库，
     # 不需要在本地额外 clone nixos-sheng。
     nixos-sheng = {
-      url = "github:DotRedstone/nixos-sheng/13c898e007a1e17b4ccea8d284986b88e4498d57?dir=nixos";
+      url = "github:DotRedstone/nixos-sheng/c9b474e607a48fe96f0d1ae3536fac7e2317b183?dir=nixos";
       inputs.shengFirmware.follows = "shengFirmware";
     };
 
