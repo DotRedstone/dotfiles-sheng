1842be7f1b3523defaa0a22743e6c6b485587de4
dotredstone <2724236945@qq.com>
2026-09-12 23:40:51 +0800

    fix(sheng): 固定 ADB gadget 交接修复

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     # 引用 sheng 硬件仓库。普通用户只需要 clone 本仓库，
     # 不需要在本地额外 clone nixos-sheng。
     nixos-sheng = {
-      url = "github:DotRedstone/nixos-sheng/82802b237501c24ea28863c19e220ce6569f715d?dir=nixos";
+      url = "github:DotRedstone/nixos-sheng/12dd60ee789c7a1a629cef684224bca722c0115e?dir=nixos";
       inputs.shengFirmware.follows = "shengFirmware";
     };
 
